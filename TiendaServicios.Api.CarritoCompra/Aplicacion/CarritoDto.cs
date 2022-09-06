a73540dc21cb4568fc2f360a140d2083d3869022
Ruri <rurik000@hotmail.com>
2022-09-06 10:54:25 -0500

    End of Section 5

---
+++ TiendaServicios.Api.CarritoCompra/Aplicacion/CarritoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TiendaServicios.Api.CarritoCompra.Aplicacion
+{
+    public class CarritoDto
+    {
+        //Comunicacion entre microservicios
+        public int CarritoId { get; set; }
+        public DateTime? FechaCreacionSesion { get; set; }
+        public List<CarritoDetalleDto> ListaProductos { get; set; }
+    }
+}
---
+++ TiendaServicios.Api.CarritoCompra/RemoteService/LibrosService.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using TiendaServicios.Api.CarritoCompra.RemoteInterface;
+using TiendaServicios.Api.CarritoCompra.RemoteModel;
+
+namespace TiendaServicios.Api.CarritoCompra.RemoteService
+{
+    public class LibrosService : ILibrosService
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly ILogger<LibrosService> _logger;
+        public LibrosService(IHttpClientFactory httpClient, ILogger<LibrosService> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public async Task<(bool resultado, LibroRemote Libro, string ErrorMessage)> GetLibro(Guid LibroId)
+        {
+            try
+            {
+                var cliente = _httpClient.CreateClient("Libros");
+                var response = await cliente.GetAsync($"api/LibroMaterial/{LibroId}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = await response.Content.ReadAsStringAsync();
+                    var optiones = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                    var resultado = JsonSerializer.Deserialize<LibroRemote>(contenido, optiones);
+                    return (true, resultado, null);
+                }
+                return (false, null, response.ReasonPhrase);
+            }
+            catch (Exception e)
+            {
+                _logger?.LogError(e.ToString());
+                return (false, null, e.Message);
+            }
+        }
+    }
+}
